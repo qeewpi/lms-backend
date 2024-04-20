2c65dc20302a82498b5a5dc73bf3a260446a36a8
qeewpi <leyashml@gmail.com>
2024-04-20 20:51:41 +0800

    initial commit

---
+++ src/test/java/com/it120p/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it120p.librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
